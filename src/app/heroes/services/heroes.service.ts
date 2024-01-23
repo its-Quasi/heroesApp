9e9fac0d3f7ec53b4730f85e37b1c0e01a4b34ad
felipe alferez <alferezfelipe@gmail.com>
2024-01-23 17:27:26 -0500

    add crud operations to hero service

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -9,10 +9,24 @@ export class HeroService {
   private url = environments.baseUrl
   constructor(private http: HttpClient) { }
 
+  // For Search Page - Component
+
+  getSuggestions(query : string) : Observable<Hero[]> {
+    const url = `${ this.url }/heroes?q=${ query }&_limit=6`
+    console.log(url)
+    return this.http.get<Hero[]>(url);
+  }
+
+  // CRUD Operations
+
+  create(hero : Hero) : Observable<Hero> {
+    return this.http.post<Hero>(`${this.url}/heroes` , hero)
+  }
+
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.url}/heroes`)
   }
-
+  
   getHeroById(id: string): Observable<Hero | undefined> {
     return this.http.get<Hero | undefined>(`${this.url}/heroes/${id}`)
       .pipe(
@@ -20,9 +34,15 @@ export class HeroService {
       )
   }
 
-  getSuggestions(query : string) : Observable<Hero[]> {
-    const url = `${ this.url }/heroes?q=${ query }&_limit=6`
-    console.log(url)
-    return this.http.get<Hero[]>(url);
+  update(hero : Hero) : Observable<Hero> {
+    if(!hero.id) throw Error('Hero dont found in database')
+    return this.http.patch<Hero>(`${this.url}/heroes/${hero.id}`, hero)
   }
+
+  deleteById(id : string) : Observable<boolean> { 
+    return this.http.delete(`${this.url}/heroes/${id}`).pipe( 
+      catchError( () => of(false) ), map( () => true ) 
+    )
+  }
+
 }
